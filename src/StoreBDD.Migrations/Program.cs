fda6d258ff45d8d1a033d9522ef73f5bffd05a03
pooria <pooriayoo09@gmail.com>
2022-04-30 21:08:15 +0430

    added initial project

---
+++ src/StoreBDD.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreBDD.Migrations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
