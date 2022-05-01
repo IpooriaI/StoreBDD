82cd470946332a7ecff52e2151f5fb30192e7a34
pooria <pooriayoo09@gmail.com>
2022-05-01 11:39:56 +0430

    added entities for category and Product and history

---
+++ src/StoreBDD.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBDD.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ src/StoreBDD.Entities/SellHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBDD.Entities
+{
+    public class SellHistory
+    {
+        public int Count { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public DateTime DateSold { get; set; }
+    }
+}
