510e4a6efb327cb0d456743662d5e78629f8e3b0
pooria <pooriayoo09@gmail.com>
2022-05-03 21:51:29 +0430

    added controller for SellFactor and BuyFactor

--- src/StoreBDD.RestAPI/Controllers/BuyFactorsController.cs
+++ src/StoreBDD.RestAPI/Controllers/BuyFactorsController.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoreBDD.Services.BuyFactors.Contracts;
+using System.Collections.Generic;
 
 namespace StoreBDD.RestAPI.Controllers
 {
+    [Route("api/buy-factors")]
+    [ApiController]
     public class BuyFactorsController : Controller
     {
+        private readonly BuyFactorService _service;
 
+        public BuyFactorsController(BuyFactorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<GetBuyFactorDto> GetAll()
+        {
+            return _service.GetAll();
+        }
     }
 }
--- src/StoreBDD.RestAPI/Controllers/SellFactorsController.cs
+++ src/StoreBDD.RestAPI/Controllers/SellFactorsController.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoreBDD.Services.SellFactors.Contracts;
+using System.Collections.Generic;
 
 namespace StoreBDD.RestAPI.Controllers
 {
+    [Route("api/sell-factors")]
+    [ApiController]
     public class SellFactorsController : Controller
     {
+        private readonly SellFactorService _service;
+
+        public SellFactorsController(SellFactorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<GetSellFactorDto> GetAll()
+        {
+            return _service.GetAll();
+        }
 
     }
 }
